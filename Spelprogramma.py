a86a462bc5ea66965287637631314785dafe93f3
MilanVD <99069122@mydavinci.nl>
2022-02-15 11:42:29 +0100

    Spelprogramma.py

---
+++ Spelprogramma.py
@@ -0,0 +1,13 @@
+import random
+AantalSpellen = random.randint(3,10)
+spelList = ['Monoploly', 'Yathzee', 'Bridge', 'Poker', 'Pesten', 'Mens erger je niet', 'Cluedo']
+print (AantalSpellen)
+
+def spelProgramma(x):
+    NieuweSpelList = []
+    for i in range(x):
+        RandomSpel = random.choice(spelList)
+        NieuweSpelList.append(RandomSpel)
+    return NieuweSpelList
+
+print (spelProgramma(AantalSpellen))
